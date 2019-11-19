dd979a40ced2662e30ba04ca1bbd22c2a5d7e47f
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2019-11-19 15:35:51 +0100

    end

--- shapes.js
+++ shapes.js
@@ -20,15 +20,23 @@ var erste = document.querySelector(".board")
 var circle1 = document.createElement("div")
 var rectangle1 = document.createElement("div")
 var parallelogram1 = document.createElement("div")
+var space = document.createElement("div")
+var egg = document.createElement("div")
+var moon = document.createElement("div")
 
 erste.appendChild(circle1)
 erste.appendChild(rectangle1)
 erste.appendChild(parallelogram1)
+erste.appendChild(space)
+erste.appendChild(egg)
+erste.appendChild(moon)
 
 circle1.classList.add("circle")
 rectangle1.classList.add("rectangle")
 parallelogram1.classList.add("parallelogram")
-
+space.classList.add("space-invader")
+egg.classList.add("egg")
+moon.classList.add("moon")
 
 circle1.style.width = circle.size + "px"
 circle1.style.height = circle.size + "px"
@@ -42,3 +50,15 @@ rectangle1.style.backgroundColor = rectangle.color
 parallelogram1.style.width = parallelogram.width + "px"
 parallelogram1.style.height = parallelogram.height + "px"
 parallelogram1.style.backgroundColor = parallelogram.color
+
+
+setInterval(function () {
+  moon.classList.toggle("moon1")
+}, 3000);
+
+
+
+
+
+
+
