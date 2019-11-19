2fd4b149b3acec66f8230786abaa3bd371a31629
Vardges-1 <vardgesbaghdasaryan93@hotmail.com>
2019-11-19 14:18:35 +0100

    circle

--- shapes.js
+++ shapes.js
@@ -1,6 +1,7 @@
 var circle = {
   size: 60,
   color: "cyan"
+
 }
 
 var rectangle = {
@@ -10,6 +11,34 @@ var rectangle = {
 
 var parallelogram = {
   width: 70,
-  height: 40
+  height: 40,
   color: "red"
 }
+
+
+var erste = document.querySelector(".board")
+var circle1 = document.createElement("div")
+var rectangle1 = document.createElement("div")
+var parallelogram1 = document.createElement("div")
+
+erste.appendChild(circle1)
+erste.appendChild(rectangle1)
+erste.appendChild(parallelogram1)
+
+circle1.classList.add("circle")
+rectangle1.classList.add("rectangle")
+parallelogram1.classList.add("parallelogram")
+
+
+circle1.style.width = circle.size + "px"
+circle1.style.height = circle.size + "px"
+circle1.style.backgroundColor = circle.color
+
+
+rectangle1.style.width = rectangle.size + "px"
+rectangle1.style.height = rectangle.size + "px"
+rectangle1.style.backgroundColor = rectangle.color
+
+parallelogram1.style.width = parallelogram.width + "px"
+parallelogram1.style.height = parallelogram.height + "px"
+parallelogram1.style.backgroundColor = parallelogram.color
